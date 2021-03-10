b62a3890eb3ff94a61b615ffe4a6dba8fca0fbfe
zacharysmith90 <70612947+zacharysmith90@users.noreply.github.com>
2021-03-09 22:31:54 -0500

    Add files via upload
    Update JS to allow filters to work independently when not showing options.

--- main.js
+++ main.js
@@ -44,7 +44,18 @@ function filter_snap() {
 	/**
 	 * Show items that match the dropdowns
 	 */
-	const filtered_items = document.querySelectorAll(`.item.${borough_class}.${month_class}.${snap_class}`);
+	let selector = ".item";
+	if (borough_class !== "") {
+		selector = `${selector}.${borough_class}`;
+	}
+	if (month_class !== "") {
+		selector = `${selector}.${month_class}`;
+	}
+	if (snap_class !== "") {
+		selector = `${selector}.${snap_class}`;
+	}
+
+	const filtered_items = document.querySelectorAll(selector);
 
 	filtered_items.forEach(function (item) {
 		item.classList.add("active");
